911feb2b61d812a4557394ea66453ba50acec2f7
Audrey Wanjiku <audreynjiraini@gmail.com>
2019-09-10 13:26:12 +0300

    create function to process results and create sources objects from the elements we need

--- app/requests.py
+++ app/requests.py
@@ -16,6 +16,7 @@ def get_sources():
 	'''
 	Function that gets the json response to url request
 	'''
+ 
 	get_sources_url = base_url.format(api_key)
 
 	with urllib.request.urlopen(get_sources_url) as url:
@@ -29,3 +30,31 @@ def get_sources():
 			sources_results = process_results(sources_results_list)
 
 	return news_results
+
+
+def process_sources(sources_list):
+	'''
+	Function that processes the news sources results and turns it into a list of objects
+	Args:
+		sources_list: A list of dictionaries that contain sources details
+	Returns:
+		sources_results: A list of sources objects
+	'''
+ 
+	sources_results = []
+
+	for source_item in sources_list:
+		id = source_item.get('id') 
+		name = source_item.get('name')
+		description = source_item.get('description')
+		url = source_item.get('url')
+		category = source_item.get('category')
+		language = source_item.get('language')
+		country = source_item.get('country')
+
+
+		sources_object = Sources(id,name,description,url,category,country,language)
+		sources_results.append(sources_object)
+
+
+	return sources_results
