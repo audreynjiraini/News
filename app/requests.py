fcac059bb35fafc8179b88c38d27122a94f0595b
Audrey Wanjiku <audreynjiraini@gmail.com>
2019-09-10 13:21:27 +0300

    add method to fetch API request

--- app/requests.py
+++ app/requests.py
@@ -10,3 +10,22 @@ def configure_request(app):
     
     api_key = app.config['NEWS_API_KEY']
     base_url = app.config['NEWS_SOURCES_BASE_URL']
+    
+    
+def get_sources():
+	'''
+	Function that gets the json response to url request
+	'''
+	get_sources_url = base_url.format(api_key)
+
+	with urllib.request.urlopen(get_sources_url) as url:
+		get_sources_data = url.read()
+		get_sources_response = json.loads(get_sources_data)
+
+		news_results = None
+
+		if get_sources_response['results']:
+			sources_results_list = get_sources_response['results']
+			sources_results = process_results(sources_results_list)
+
+	return news_results
