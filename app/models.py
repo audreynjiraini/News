59569955fbd4e0415753658214dbae680cf34d5f
Audrey Wanjiku <audreynjiraini@gmail.com>
2019-09-10 13:11:52 +0300

    create Sources class

---
+++ app/models.py
@@ -0,0 +1,13 @@
+class Sources:
+    '''
+    Sources class to define sources objects
+    '''
+    
+    def __init__(self, id, name, description, url, category, language):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
+        self.country = country
+        self.language = language
