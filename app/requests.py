57f02ba7f4276e26e3609c59923f9485a23106b4
Audrey Wanjiku <audreynjiraini@gmail.com>
2019-09-10 18:54:42 +0300

    display articles

--- app/requests.py
+++ app/requests.py
@@ -5,11 +5,14 @@ api_key = None
 
 base_url = None
 
+articles_url = None
+
 def configure_request(app):
-    global api_key, base_url
+    global api_key, base_url, articles_url
     
     api_key = app.config['NEWS_API_KEY']
     base_url = app.config['NEWS_SOURCES_BASE_URL']
+    articles_url = app.config['ARTICLES_BASE_URL']
     
     
 def get_sources(category):
