f1b85e4d2e32f1450b1f8c880a9b3890f0540244
Audrey Wanjiku <audreynjiraini@gmail.com>
2019-09-10 16:01:40 +0300

    create get articles and process articles functions

--- app/requests.py
+++ app/requests.py
@@ -58,3 +58,48 @@ def process_sources(sources_list):
 
 
 	return sources_results
+
+
+def get_articles(id):
+	'''
+	Function that processes the articles and returns a list of articles objects
+	'''
+ 
+	get_articles_url = articles_url.format(id,api_key)
+
+	with urllib.request.urlopen(get_articles_url) as url:
+		articles_results = json.loads(url.read())
+
+
+		articles_object = None
+		if articles_results['articles']:
+			articles_object = process_articles(articles_results['articles'])
+
+	return articles_object
+
+
+def process_articles(articles_list):
+	'''
+    Function that processes the articles results and turns it into a list of objects
+	Args:
+		articles_list: A list of dictionaries that contain articles details
+	Returns:
+		articles_results: A list of articles objects
+    '''
+    
+	articles_object = []
+ 
+	for article_item in articles_list:
+		id = article_item.get('id')
+		author = article_item.get('author')
+		title = article_item.get('title')
+		description = article_item.get('description')
+		url = article_item.get('url')
+		image = article_item.get('urlToImage')
+		date = article_item.get('publishedAt')
+		
+		if image:
+			articles_result = Articles(id,author,title,description,url,image,date)
+			articles_object.append(articles_result)	
+
+	return articles_object
